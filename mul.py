f5dfe3e45ff669f8636acc52d9befe364a0c70cc
durgadurgesh <durgaduttdurgesh2002@gmail.com>
2025-03-28 13:08:01 +0530

    add new file

--- mul.py
+++ mul.py
@@ -1,4 +1,4 @@
-class Adder:
+class multiplication:
     def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
@@ -11,7 +11,7 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 # Creating an object of the class
-addition = Adder(num1, num2)
+mul = multiplication(num1, num2)
 
 # Displaying the result
-print(f"The sum of {num1} and {num2} is: {addition.add()}")
+print(f"The sum of {num1} and {num2} is: {mul.add()}")
